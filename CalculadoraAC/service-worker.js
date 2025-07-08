733e650da699c416d6c58af860cef5cff3c8c09f
jereefecto <jeremiasce1@gmail.com>
2025-07-08 03:33:21 -0400

    Delete CalculadoraAC/service-worker.js

--- CalculadoraAC/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', event => {
-  // Puedes mejorar esto más adelante para caché offline
-});
